703de858d6ed14f12fbcf80db0fd804d4d0975a9
Михаил <sidorkin-misha93@mail.ru>
2023-11-08 20:23:20 +0300

    Добавил проверку текста в поиске с первого урока.

--- test_google_search.py
+++ test_google_search.py
@@ -8,9 +8,11 @@ def test_google_find_selene():
     browser.element('[id="search"]').should(have.text('Selene - User-oriented Web UI browser tests in Python'))
     browser.quit()
 
+
 def test_google_should_not_find_selene():
     browser.open('https://google.com')
     browser.element('[name="q"]').should(be.blank).type('ikasnmf;m[asv[pks[pal,xcsn aps;np[kqw;').press_enter()
-    browser.element('#botstuff').should(have.text('По запросу ikasnmf;m[asv[pks[pal,xcsn aps;np[kqw; ничего не найдено.'))
+    browser.element('#botstuff').should(
+        have.text('По запросу ikasnmf;m[asv[pks[pal,xcsn aps;np[kqw; ничего не найдено.'))
 
     ...
